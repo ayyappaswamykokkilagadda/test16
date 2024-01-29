e3df48ca0fc8aff8e7fc1fd631b05fc3c1b7d21d
Looser <koradasai.ss@gmail.com>
2024-01-29 12:50:32 +0530

    for you

--- coding2.java
+++ coding2.java
@@ -2,7 +2,7 @@ class evilnumber
 {
    public static void main(String[]args)
    {
-     int numb=45,rem=0,square,sum=0;
+     int numb=62,rem=0,square,sum=0;
      square=(int)Math.pow(numb,2);
 
      for(;square>0;square/=10)
